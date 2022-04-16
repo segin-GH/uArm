774a42e7f9f8c0403950d9819498e8ef52639bae
SEGIN-zz <segin.bytes@gmail.com>
2022-04-16 17:48:41 +0530

    .

--- uarm/src/main.cpp
+++ uarm/src/main.cpp
@@ -1,20 +1,32 @@
 # include <Arduino.h>
-# include <Servo.h>
+# include <Servo.h>              
+# include<Stepper.h>
 
-Servo servoZero;
-int servoZeroPin = 2;
-Servo servoOne;
-int servoOnePin = 3;
+int spr = 2048;
+const uint8_t mot_speed = 16;
+
+Servo servo_griper;
+Servo servo_upperArm;
+Servo servo_forArm;
+Stepper stepper_base (spr,8,10,9,11);
+
+
+int servo_upperArm_degree = 90;
+int servo_forArm_degree = 90;
+int servo_gripper_degree = 90;
+int stepper_base_degree;
 
 void setup()
 {
   Serial.begin(9600);
-  servoZero.attach(servoZeroPin);
-  servoOne.attach(servoOnePin);
+  servo_upperArm.attach(2);
+  servo_forArm.attach(3);
+  servo_griper.attach(4);
+  stepper_base.setSpeed(mot_speed);
+
 }
 
 void loop()
 {
-  servoZero.write(70);
-  servoOne.write(90);
+  //
 }
