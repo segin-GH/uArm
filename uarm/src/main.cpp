1472d81e41ec90a1edc29331042c36efe9121ad7
SEGIN-zz <segin.bytes@gmail.com>
2022-04-16 18:05:10 +0530

    .

--- uarm/src/main.cpp
+++ uarm/src/main.cpp
@@ -16,6 +16,12 @@ int servo_forArm_degree = 90;
 int servo_gripper_degree = 90;
 int stepper_base_degree;
 
+const uint8_t left_joystick_x = A0;
+const uint8_t left_joystick_y = A1;
+const uint8_t right_joystick_x = A2;
+const uint8_t right_joystick_y = A3;
+
+
 void setup()
 {
   Serial.begin(9600);
@@ -24,9 +30,18 @@ void setup()
   servo_griper.attach(4);
   stepper_base.setSpeed(mot_speed);
 
+  pinMode(left_joystick_x,INPUT);
+  pinMode(left_joystick_y,INPUT);
+  pinMode(right_joystick_x,INPUT);
+  pinMode(right_joystick_y,INPUT);
+
+
 }
 
 void loop()
 {
-  //
+  int left_joystick_x_value = analogRead(left_joystick_x);
+  int left_joystick_y_value = analogRead(left_joystick_y);
+  int right_joystick_x_value = analogRead(right_joystick_x);
+  int right_joystick_y_value = analogRead(right_joystick_y);
 }
