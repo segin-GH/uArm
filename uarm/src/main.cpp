e4b3025b254ead1f570800a0f47d7446f2f91925
SEGIN-zz <segin.bytes@gmail.com>
2022-04-17 20:05:41 +0530

    .

--- uarm/src/main.cpp
+++ uarm/src/main.cpp
@@ -2,31 +2,31 @@
 # include <Servo.h>              
 # include<Stepper.h>
 
-int spr = 2048;
-const uint8_t mot_speed = 16;
+  int spr = 2048;
+  const uint8_t mot_speed = 16;
 
-Servo servo_griper;
-Servo servo_upperArm;
-Servo servo_forArm;
-Stepper stepper_base (spr,8,10,9,11);
+  Servo servo_griper;
+  Servo servo_upperArm;
+  Servo servo_foreArm;
+  Stepper stepper_base (spr,8,10,9,11);
 
 
-int servo_upperArm_degree = 90;
-int servo_forArm_degree = 90;
-int servo_gripper_degree = 90;
-int stepper_base_degree;
+  int servo_upperArm_degree = 90;
+  int servo_foreArm_degree = 90;
+  int servo_gripper_degree = 90;
+  int stepper_base_degree;
 
-const uint8_t left_joystick_x = A0;
-const uint8_t left_joystick_y = A1;
-const uint8_t right_joystick_x = A2;
-const uint8_t right_joystick_y = A3;
+  const uint8_t left_joystick_x = A0;
+  const uint8_t left_joystick_y = A1;
+  const uint8_t right_joystick_x = A2;
+  const uint8_t right_joystick_y = A3;
 
 
 void setup()
 {
   Serial.begin(9600);
   servo_upperArm.attach(2);
-  servo_forArm.attach(3);
+  servo_foreArm.attach(3);
   servo_griper.attach(4);
   stepper_base.setSpeed(mot_speed);
 
@@ -44,7 +44,40 @@ void loop()
   int left_joystick_y_value = analogRead(left_joystick_y);
   int right_joystick_x_value = analogRead(right_joystick_x);
   int right_joystick_y_value = analogRead(right_joystick_y);
-  
 
+    // **********************Left Joystick Logic*******************************
+
+  if (left_joystick_x_value < 350)
+  {
+    servo_upperArm_degree = servo_upperArm_degree - 7;
+  }
+  else if (left_joystick_x_value > 680)
+  {
+    servo_upperArm_degree = servo_upperArm_degree + 7;
+  }
+
+
+  if (left_joystick_y_value < 350)
+  {
+    stepper_base.step(-1);
+  }
+  else if (left_joystick_y_value > 680)
+  {
+    stepper_base.step(+1);
+  }
+
+    // ************************Right Joystick Logic*******************************
+
+  if (right_joystick_x_value < 350)
+  {
+    servo_foreArm_degree = servo_foreArm_degree - 7;
+  }
+  else if (right_joystick_x_value > 680)
+  {
+    servo_foreArm_degree = servo_foreArm_degree + 7;
+  }
+     // **************************** End Limts Logic *******************************
+
+  
 
 }
