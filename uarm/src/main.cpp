57a247bc8e174a125aad02698a3ea3bb2220201b
im-segin <segin.bytes@gmail.com>
2022-05-26 16:25:34 +0530

    .

--- uarm/src/main.cpp
+++ uarm/src/main.cpp
@@ -1,83 +1,11 @@
-# include <Arduino.h>
-# include <Servo.h>              
-# include<Stepper.h>
-
-  int spr = 2048;
-  const uint8_t mot_speed = 16;
-
-  Servo servo_griper;
-  Servo servo_upperArm;
-  Servo servo_foreArm;
-  Stepper stepper_base (spr,8,10,9,11);
-
-
-  int servo_upperArm_degree = 90;
-  int servo_foreArm_degree = 90;
-  int servo_gripper_degree = 90;
-  int stepper_base_degree;
-
-  const uint8_t left_joystick_x = A0;
-  const uint8_t left_joystick_y = A1;
-  const uint8_t right_joystick_x = A2;
-  const uint8_t right_joystick_y = A3;
-
-
-void setup()
-{
-  Serial.begin(9600);
-  servo_upperArm.attach(2);
-  servo_foreArm.attach(3);
-  servo_griper.attach(4);
-  stepper_base.setSpeed(mot_speed);
-
-  pinMode(left_joystick_x,INPUT);
-  pinMode(left_joystick_y,INPUT);
-  pinMode(right_joystick_x,INPUT);
-  pinMode(right_joystick_y,INPUT);
-
-
-}
-
-void loop()
-{
-  int left_joystick_x_value = analogRead(left_joystick_x);
-  int left_joystick_y_value = analogRead(left_joystick_y);
-  int right_joystick_x_value = analogRead(right_joystick_x);
-  int right_joystick_y_value = analogRead(right_joystick_y);
-
-    // **********************Left Joystick Logic*******************************
-
-  if (left_joystick_x_value < 350)
-  {
-    servo_upperArm_degree = servo_upperArm_degree - 7;
-  }
-  else if (left_joystick_x_value > 680)
-  {
-    servo_upperArm_degree = servo_upperArm_degree + 7;
-  }
-
-
-  if (left_joystick_y_value < 350)
-  {
-    stepper_base.step(-1);
-  }
-  else if (left_joystick_y_value > 680)
-  {
-    stepper_base.step(+1);
-  }
-
-    // ************************Right Joystick Logic*******************************
-
-  if (right_joystick_x_value < 350)
-  {
-    servo_foreArm_degree = servo_foreArm_degree - 7;
-  }
-  else if (right_joystick_x_value > 680)
-  {
-    servo_foreArm_degree = servo_foreArm_degree + 7;
-  }
-     // **************************** End Limts Logic *******************************
-
-  
-
-}
+/* 
+ *  src code 4 uArm
+ *    main.cpp
+ * 
+ *  Created on: 2022-May-26
+ *        Dev : segin
+ * 
+ *  Ongoing-devlopment
+ * 
+ *  GNU General Public License v3.0 
+ */
