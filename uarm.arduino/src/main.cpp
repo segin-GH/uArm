619676d80d08e2ccf5b67b05a081232fca85e115
im-segin <segin.bytes@gmail.com>
2022-05-29 20:35:36 +0530

    .

---
+++ uarm.arduino/src/main.cpp
@@ -0,0 +1,45 @@
+/* 
+ *  src code 4 uArm
+ *    main.cpp
+ * 
+ *  Created on: 2022-May-26
+ *        Dev : segin
+ * 
+ *  Ongoing-devlopment
+ * 
+ *  GNU General Public License v3.0 
+ */
+
+# include <Arduino.h>
+# include <Stepper.h>
+# include <Servo.h>
+
+const uint16_t spr = 2048;
+const uint8_t stepperSpeed = 10;
+Stepper baseStepper (spr,8,10,9,11);
+
+const uint8_t foreArmServoPin = 2;
+const uint8_t upperArmServoPin = 3;
+Servo upperArmServo;
+Servo foreArmServo;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  baseStepper.setSpeed(stepperSpeed);
+  upperArmServo.attach(upperArmServoPin);
+  foreArmServo.attach(foreArmServoPin);
+
+}
+void loop ()
+{
+   
+}
+
+int getCoordinates (uint8_t x, uint8_t y, uint8_t z)
+{
+  Serial.print("Enter the X coordinate : ");
+  
+  
+}
