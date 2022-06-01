6bde536b4278aeeacae45e7c16cc56e87a3217e4
segin-GH <segin.bytes@gmail.com>
2022-06-02 01:43:32 +0530

    .

--- uArm.arduino/src/main.cpp
+++ uArm.arduino/src/main.cpp
@@ -13,31 +13,72 @@
 # include <Arduino.h>
 # include <Stepper.h>
 # include <Servo.h>
+# include <string.h>
 
-const uint16_t spr = 2048;
-const uint8_t stepperSpeed = 10;
-Stepper baseStepper (spr,8,10,9,11);
+// const uint16_t spr = 2048;
+// const uint8_t stepperSpeed = 10;
+// Stepper baseStepper (spr,8,10,9,11);
 
-const uint8_t foreArmServoPin = 2;
-const uint8_t upperArmServoPin = 3;
-Servo upperArmServo;
-Servo foreArmServo;
+// const uint8_t foreArmServoPin = 2;
+// const uint8_t upperArmServoPin = 3;
+// Servo upperArmServo;
+// Servo foreArmServo;
 
+String reciveArduinoData;
+
+void sendArduinoData(String data);
+
+bool autoHome();
+bool autoHomeStatus;
 
 void setup()
 {
-  Serial.begin(9600);
-  baseStepper.setSpeed(stepperSpeed);
-  upperArmServo.attach(upperArmServoPin);
-  foreArmServo.attach(foreArmServoPin);
+  Serial.begin(115200);
+  //Serial.setTimeout(10);
+  // baseStepper.setSpeed(stepperSpeed);
+  // upperArmServo.attach(upperArmServoPin);
+  // foreArmServo.attach(foreArmServoPin);
+  pinMode(13,OUTPUT);
 
 }
 void loop ()
 {
-   
+
+  while(Serial.available() == 0)
+  {
+    // do nothing
+  }
+
+  reciveArduinoData = Serial.readStringUntil('\r');
+  //char firstcharacter = reciveArduinoData[0];
+
+    if (reciveArduinoData == "G10")
+    {
+      autoHomeStatus = autoHome();
+        if (autoHomeStatus == true)
+        {
+          sendArduinoData("AUTO HOME COMPLET.");
+        }
+    }
+
+  
+  
+
+
+
+
+
 }
 
-int autoHome ()
+
+
+bool autoHome()
 {
+  digitalWrite(13,HIGH);
   
+  return true;
+}
+void sendArduinoData(String data)
+{
+  Serial.println(data);
 }
--- uArm.python/uArm.py
+++ uArm.python/uArm.py
@@ -1,22 +1,48 @@
-from time import*
-from serial import*
-from threading import*
+
+import time
+import serial
 
 
+sendArdData = serial.Serial('/dev/ttyACM0',115200)
+reciveArdData = serial.Serial('/dev/ttyACM0',115200)
+time.sleep(1)
+
+
+# def sendArduinoData(sendData):
+#     sendData = sendData + '\r'
+#     print(sendData)
+#     sendArdData.write(sendData.encode())
+
+# def reciveArduinoData():
+#     while(reciveArdData.inWaiting() == 0):
+#         pass
+#     reciveDataPacket = reciveArdData.readline()
+#     reciveDataPacket = str(reciveDataPacket,'utf-8')
+#     reciveDataPacket = reciveDataPacket.strip('\r\n')
+#     return reciveDataPacket
 
 
 while True:
+
     cmd = (input("Enter cmd : "))
 
     if (cmd == "G10"):
         print(" OK.")
-        print("INITIALIZING AUTOHOME")
+        print(" INITIALIZING AUTO HOME ")
+        cmd = cmd + ('\r')
+        sendArdData.write(cmd.encode())
+
+        
 
     if (cmd == "M20"):
         print(" OK.")
         print("INITIALIZING MOVE ONCE")
 
+
+
     if (cmd == "M30"):
         print(" OK.")
         print("INITIALIZING MOVE INFINITE")
+
+
 
